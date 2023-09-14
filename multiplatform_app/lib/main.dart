5dca8ea7dbe8646d5aa54db2ef8c965849c54134
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2023-09-15 02:07:16 +0800

    1

--- multiplatform_app/lib/main.dart
+++ multiplatform_app/lib/main.dart
@@ -1,14 +1,16 @@
 import 'dart:io';
 
-import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+
 import 'package:html/parser.dart' as htmlparser;
-import 'package:webview_all/webview_all.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
+void main() {
   runApp(const MainApp());
 }
 
@@ -26,19 +28,57 @@ class _MainAppState extends State<MainApp> {
 
   String siteHTML = "siteHTML";
 
+  bool isLoaded = false;
+
   final _controller = TextEditingController();
+  late final WebViewController _wvcontroller;
 
   @override
   void initState() {
     _controller.text = 'https://flutter.dev';
 
-    super.initState();
-  }
+    late final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+    _wvcontroller = WebViewController.fromPlatformCreationParams(params);
+    // #enddocregion platform_features
+
+    _wvcontroller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://flutter.dev'));
+
+    // #docregion platform_features
+    if (_wvcontroller.platform is AndroidWebViewController) {
+      AndroidWebViewController.enableDebugging(true);
+      (_wvcontroller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+    }
+
+    // _wvcontroller = WebViewController()
+    //   ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    //   ..setNavigationDelegate(
+    //     NavigationDelegate(
+    //       onProgress: (int progress) {
+    //         debugPrint("Loading: $progress%");
+    //       },
+    //       onPageStarted: (String url) {},
+    //       onPageFinished: (String url) {},
+    //       onWebResourceError: (WebResourceError error) {},
+    //       onNavigationRequest: (NavigationRequest request) {
+    //         return NavigationDecision.navigate;
+    //       },
+    //     ),
+    //   )
+    //   ..loadRequest(Uri.parse(_controller.text));
+
+    super.initState();
   }
 
   Future<(String, String, String)> _loadHtmlPage() async {
@@ -73,9 +113,9 @@ class _MainAppState extends State<MainApp> {
                   style: const TextStyle(color: Colors.red, fontSize: 16)),
               Expanded(
                 child: SingleChildScrollView(
-//                  child: Text(siteHTML),
-                  child: Webview(
-                    url: _controller.text,
+                  //child: Text(siteHTML),
+                  child: WebViewWidget(
+                    controller: _wvcontroller,
                   ),
                 ),
               ),
