7b379239c0e3b4fd3d0ca14b339c00467cc7bf1a
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2023-09-14 20:42:29 +0800

    1

--- multiplatform_app/lib/main.dart
+++ multiplatform_app/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb;
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_html/html_parser.dart';
+
+import 'package:html/parser.dart' as htmlparser;
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MainApp());
@@ -23,17 +26,40 @@ class _MainAppState extends State<MainApp> {
   bool isLoaded = false;
 
   final _controller = TextEditingController();
+  final _wvcontroller = WebViewController();
 
   @override
   void initState() {
     _controller.text = 'https://flutter.dev';
+
+    _wvcontroller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            debugPrint("Loading: $progress%");
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(_controller.text));
+
     super.initState();
   }
 
   Future<(String, String, String)> _loadHtmlPage() async {
     final response = await Dio().get(_controller.text);
-    final htmldoc = HtmlParser.parseHTML(response.data);
-    return (htmldoc.body.toString(), htmldoc.head.toString(), "");
+    final htmldoc = htmlparser.parse(response.data);
+    return (
+      response.data.toString(),
+      htmldoc.body!.getElementsByTagName('h1')[0].innerHtml,
+      response.headers.toString()
+    );
   }
 
   @override
@@ -44,9 +70,20 @@ class _MainAppState extends State<MainApp> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(siteHeader),
-            Text(siteResponseHeader),
-            Expanded(child: SingleChildScrollView(child: Text(siteHTML))),
+            Text(defaultTargetPlatform.toString(),
+                style:
+                    const TextStyle(fontSize: 12, fontWeight: FontWeight.bold)),
+            Text(siteHeader,
+                style:
+                    const TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+            Text(siteResponseHeader,
+                style: const TextStyle(color: Colors.red, fontSize: 16)),
+            Expanded(
+              child: SingleChildScrollView(
+                  child: WebViewWidget(
+                controller: _wvcontroller,
+              )),
+            ),
             Row(children: [
               Expanded(
                 child: SizedBox(
@@ -62,6 +99,7 @@ class _MainAppState extends State<MainApp> {
                     setState(() {
                       siteHTML = response.$1;
                       siteHeader = response.$2;
+                      siteResponseHeader = response.$3;
                     });
                   },
                   child: const Text('Загрузить сайт'))
