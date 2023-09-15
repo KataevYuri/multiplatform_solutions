e5dc0ca69bd9b3ccf2f93cd7c928c309057ae1aa
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2023-09-15 09:47:41 +0800

    1

--- multiplatform_app/lib/main.dart
+++ multiplatform_app/lib/main.dart
@@ -5,10 +5,6 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 import 'package:html/parser.dart' as htmlparser;
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_flutter_android/webview_flutter_android.dart';
-import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
-import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
 
 void main() {
   runApp(const MainApp());
@@ -31,54 +27,18 @@ class _MainAppState extends State<MainApp> {
   bool isLoaded = false;
 
   final _controller = TextEditingController();
-  late final WebViewController _wvcontroller;
 
   @override
   void initState() {
     _controller.text = 'https://flutter.dev';
 
-    late final PlatformWebViewControllerCreationParams params;
-    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
-      params = WebKitWebViewControllerCreationParams(
-        allowsInlineMediaPlayback: true,
-        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
-      );
-    } else {
-      params = const PlatformWebViewControllerCreationParams();
-    }
-
-    _wvcontroller = WebViewController.fromPlatformCreationParams(params);
-    // #enddocregion platform_features
-
-    _wvcontroller
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse('https://flutter.dev'));
-
-    // #docregion platform_features
-    if (_wvcontroller.platform is AndroidWebViewController) {
-      AndroidWebViewController.enableDebugging(true);
-      (_wvcontroller.platform as AndroidWebViewController)
-          .setMediaPlaybackRequiresUserGesture(false);
-    }
-
-    // _wvcontroller = WebViewController()
-    //   ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    //   ..setNavigationDelegate(
-    //     NavigationDelegate(
-    //       onProgress: (int progress) {
-    //         debugPrint("Loading: $progress%");
-    //       },
-    //       onPageStarted: (String url) {},
-    //       onPageFinished: (String url) {},
-    //       onWebResourceError: (WebResourceError error) {},
-    //       onNavigationRequest: (NavigationRequest request) {
-    //         return NavigationDecision.navigate;
-    //       },
-    //     ),
-    //   )
-    //   ..loadRequest(Uri.parse(_controller.text));
-
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   Future<(String, String, String)> _loadHtmlPage() async {
@@ -113,10 +73,7 @@ class _MainAppState extends State<MainApp> {
                   style: const TextStyle(color: Colors.red, fontSize: 16)),
               Expanded(
                 child: SingleChildScrollView(
-                  //child: Text(siteHTML),
-                  child: WebViewWidget(
-                    controller: _wvcontroller,
-                  ),
+                  child: Text(siteHTML),
                 ),
               ),
               Row(children: [
